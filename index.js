57e647a3426199e8252a4b1113843b556ad880cf
Julius <mail@juliuste.de>
2017-08-20 07:26:56 +0200

    HTTPS -> HTTP for nginx integration

--- index.js
+++ index.js
@@ -3,10 +3,8 @@
 const config       = require('config')
 const fs           = require('fs')
 const express      = require('express')
-const spdy         = require('spdy')
 const corser       = require('corser')
 const http		   = require('http')
-const forceSSL 	   = require('express-force-ssl')
 const compression  = require('compression')
 const nocache      = require('nocache')
 const path         = require('path')
@@ -14,24 +12,9 @@ const bodyParser   = require('body-parser')
 
 const query = require('./query')
 
-// configure SSL
-const ssl = {
-	  key:  fs.readFileSync(config.key)
-	, cert: fs.readFileSync(config.cert)
-	, ca:   fs.readFileSync(config.ca)
-}
-
 // setup HTTP and HTTPS servers
 const api = express()
-const httpApi = express()
-const httpServer = http.createServer(httpApi)
-const server = spdy.createServer(ssl, api)
-
-// force SSL usage
-httpApi.use(forceSSL)
-httpApi.set('forceSSLOptions', {
-  httpsPort: config.port
-})
+const server = http.createServer(api)
 
 const allowed = corser.simpleRequestHeaders.concat(['User-Agent'])
 api.use(corser.create({requestHeaders: allowed})) // CORS
@@ -48,14 +31,8 @@ api.use('/assets', express.static('assets'))
 // set routes
 api.post('/', query)
 
-// start HTTPS server
+// start HTTP server
 server.listen(config.port, (e) => {
 	if (e) return console.error(e)
-	console.log(`HTTPS: Listening on ${config.port}.`)
-})
-
-// start HTTP server
-httpServer.listen(config.httpPort, (e) => {
-	if (e) return console.error(e)
-	console.log(`HTTP: Listening on ${config.httpPort}.`)
+	console.log(`HTTP: Listening on ${config.port}.`)
 })
